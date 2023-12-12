c2263185d44b62f0a3c4c04de2a4a198079291aa
Jorge de los Santos <tebayoso@gmail.com>
2023-12-12 14:56:08 -0300

    Added test for ownable contract

--- test/Migration.ts
+++ test/Migration.ts
@@ -49,11 +49,18 @@ describe("PoXMigration deploy", function () {
     const { myMigration } = await loadFixture(deployFixture);
     const [deployerWallet, depositerWallet] = await hre.viem.getWalletClients();
 
-    // change the signer to the depositerWallet
-    // what do I do here?
+    // import the contract with the depositerWallet
+    const myMigrationDepositer = await hre.viem.getContractAt("PoXMigration", myMigration.address, { walletClient: depositerWallet});
 
-    // check that the migration is started
-    const started = await myMigration.read.isMigrationActive();
-    assert.isFalse(started);
-  }
+    // Attempt to start the migration in the myMigrationDepositers contract, and capture the rpc error that is thrown to validate Ownable
+    let error: any;
+    try {
+      await myMigrationDepositer.write.startMigration();
+    } catch (err) {
+      // assert that the error is OwnableUnauthorizedAccount with the proper address
+      error = err;
+    }
+    assert.isTrue(error.message.includes("OwnableUnauthorizedAccount"));
+
+  })
 });
