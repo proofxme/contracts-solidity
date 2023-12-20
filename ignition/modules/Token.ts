8c623aa1e600693291f23869b293d8536cf1f3a4
Jorge de los Santos <tebayoso@gmail.com>
2023-12-20 17:42:36 -0300

    Setup Hardhat Ignition and configured the first module to deploy tokens

--- ignition/modules/Token.ts
+++ ignition/modules/Token.ts
@@ -4,5 +4,9 @@ const TokenModule = buildModule("Token", (m) => {
   const deployerWallet = m.getAccount(0);
   const myToken = m.contract("ProofOfX", [deployerWallet, deployerWallet]);
 
+  m.call(myToken, "name", [])
+
   return {myToken};
 });
+
+export default TokenModule
