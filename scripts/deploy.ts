6ae67f11a13be070dd9def75a4b5e2d0c4282a40
Jorge de los Santos <tebayoso@gmail.com>
2024-01-04 18:14:11 -0300

    Added base scripts

--- scripts/deploy.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const TokenModule = buildModule("Token", (m) => {
-  const deployerWallet = m.getAccount(0);
-  const myToken = m.contract("ProofOfX", [deployerWallet, deployerWallet]);
-
-  return {myToken};
-});
